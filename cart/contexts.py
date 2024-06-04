331206f9d2c4f05c6c5d83fb5f49f1e39a8d3a24
barry flynn <barry.c.flynn@gmail.com>
2024-06-04 13:43:43 +0000

    added cart fucntionality to work with size and inventoryitem models

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,14 +1,40 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from inventory.models import InventoryItem, Size
 
 def cart_contents(request):
-
     cart_items = []
     total = 0
     inventory_item_count = 0
+    cart = request.session.get('cart', {})
 
-    if total > settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVEYRY_PERCENTAGE / 100)
+    for item_id, item_data in cart.items():
+        inventory_item = get_object_or_404(InventoryItem, pk=item_id)
+        if isinstance(item_data, dict) and 'items_by_size' in item_data:
+            for size_id, quantity in item_data['items_by_size'].items():
+                size = get_object_or_404(Size, pk=size_id)
+                total += quantity * size.price
+                inventory_item_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'size': size_id,
+                    'quantity': quantity,
+                    'inventory_item': inventory_item,
+                    'size_price': size.price,
+                })
+        else:
+            # If no size is associated, default to a price if applicable
+            cart_items.append({
+                'item_id': item_id,
+                'size': None,
+                'quantity': item_data,
+                'inventory_item': inventory_item,
+                'size_price': 0,  # Default to 0 or some logic to handle non-size items
+            })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib import messages
+from inventory.models import InventoryItem, Size
 
 # Create your views here.
 
@@ -7,19 +9,26 @@ def view_cart(request):
 
     return render(request, 'cart/cart.html')
 
-
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
-
+    inventory_item = get_object_or_404(InventoryItem, pk=item_id)
     quantity = int(request.POST.get('quantity'))
+    size_id = request.POST.get('size')
+    size = get_object_or_404(Size, pk=size_id)
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    # Ensure the item is stored as a dictionary with items_by_size key
+    if item_id in cart:
+        if not isinstance(cart[item_id], dict):
+            cart[item_id] = {'items_by_size': {}}
+        if size_id in cart[item_id]['items_by_size']:
+            cart[item_id]['items_by_size'][size_id] += quantity
+        else:
+            cart[item_id]['items_by_size'][size_id] = quantity
     else:
-        cart[item_id] = quantity
+        cart[item_id] = {'items_by_size': {size_id: quantity}}
 
     request.session['cart'] = cart
-    print(request.session['cart'])
+    messages.success(request, f'Added {quantity} x {inventory_item.name} (Size: {size.get_size_display()}) to your cart')
     return redirect(redirect_url)
