1b720db7eb27ad9120becb51c702e6ac386c9555
barry flynn <barry.c.flynn@gmail.com>
2024-06-09 16:54:44 +0000

    add signals to update orders with new line items

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class OrdersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class CheckoutConfig(AppConfig):
     name = 'orders'
+
+    def ready(self):
+        import orders.signals
