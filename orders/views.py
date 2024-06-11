10af0fe6a4379a459bff8aa5856883c12ba35239
barry flynn <barry.c.flynn@gmail.com>
2024-06-11 09:10:33 +0000

    fixed order_success showing, return incorrectly set

--- orders/views.py
+++ orders/views.py
@@ -49,7 +49,6 @@ def orders(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
@@ -98,25 +97,26 @@ def orders(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'full_name': profile.user.get_full_name(),
-                    'email': profile.user.email,
-                    'phone_number': profile.default_phone_number,
-                    'country': profile.default_country,
-                    'postcode': profile.default_postcode,
-                    'town_or_city': profile.default_town_or_city,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
-                    'county': profile.default_county,
-                })
-            except UserProfile.DoesNotExist:
-                order_form = OrderForm()
-        else:
-            order_form = OrderForm()
+        order_form = OrderForm()
+        
+        # if request.user.is_authenticated:
+        #     try:
+        #         profile = UserProfile.objects.get(user=request.user)
+        #         order_form = OrderForm(initial={
+        #             'full_name': profile.user.get_full_name(),
+        #             'email': profile.user.email,
+        #             'phone_number': profile.default_phone_number,
+        #             'country': profile.default_country,
+        #             'postcode': profile.default_postcode,
+        #             'town_or_city': profile.default_town_or_city,
+        #             'street_address1': profile.default_street_address1,
+        #             'street_address2': profile.default_street_address2,
+        #             'county': profile.default_county,
+        #         })
+        #     except UserProfile.DoesNotExist:
+        #         order_form = OrderForm()
+        # else:
+        #     order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -139,26 +139,26 @@ def order_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
+    # if request.user.is_authenticated:
+    #     profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
-        order.user_profile = profile
-        order.save()
+        # order.user_profile = profile
+        # order.save()
 
         # Save the user's info
-        if save_info:
-            profile_data = {
-                'default_phone_number': order.phone_number,
-                'default_country': order.country,
-                'default_postcode': order.postcode,
-                'default_town_or_city': order.town_or_city,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
-                'default_county': order.county,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
+        # if save_info:
+        #     profile_data = {
+        #         'default_phone_number': order.phone_number,
+        #         'default_country': order.country,
+        #         'default_postcode': order.postcode,
+        #         'default_town_or_city': order.town_or_city,
+        #         'default_street_address1': order.street_address1,
+        #         'default_street_address2': order.street_address2,
+        #         'default_county': order.county,
+        #     }
+        #     user_profile_form = UserProfileForm(profile_data, instance=profile)
+        #     if user_profile_form.is_valid():
+        #         user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
@@ -172,4 +172,4 @@ def order_success(request, order_number):
         'order': order,
     }
 
-    return render(request, template, context)
+    return render(request, 'orders/order_success.html', context)
