29013c136d00a1a93ae15cf909392a27e7b5b505
barry flynn <barry.c.flynn@gmail.com>
2024-06-13 22:32:33 +0000

    removed security key and added dubug check

--- pixel_perfect/settings.py
+++ pixel_perfect/settings.py
@@ -26,10 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e!ux^-r9ozhq5bg=zqw#z9)+&j##5-&vyst@3@uzjvb+q9e3da'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
+
 
 ALLOWED_HOSTS = [
                 '8000-barrycflynn-pixelperfec-txa16ud0xsf.ws-eu114.gitpod.io',
