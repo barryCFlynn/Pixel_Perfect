d7078b97388d3ed200b6ad499f04229679752f9e
barry flynn <barry.c.flynn@gmail.com>
2024-06-14 16:07:22 +0000

    wip adding email setup

--- pixel_perfect/settings.py
+++ pixel_perfect/settings.py
@@ -117,8 +117,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -211,4 +209,8 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 CLOUDINARY_URL = os.getenv('CLOUDINARY_URL', '')
-DEFAULT_FROM_EMAIL = 'mypixelperfectofficial@gmail.com'
+
+
+if 'DEVELOPMENT' in os.environ: 
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'mypixelperfectofficial@example.com'
